7cff7f38e6e3655265cebd46b463222f51d949ca
MalakAlyazji <161591417+MalakAlyazji@users.noreply.github.com>
2024-03-10 21:31:46 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace NumberingSystemProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.from = new System.Windows.Forms.ComboBox();
+            this.to = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btstart = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(219, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 94);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numbers System ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // from
+            // 
+            this.from.Enabled = false;
+            this.from.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.from.FormattingEnabled = true;
+            this.from.Items.AddRange(new object[] {
+            "Binary",
+            "Decimal",
+            "Octal",
+            "Hexadecimal"});
+            this.from.Location = new System.Drawing.Point(85, 327);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(218, 32);
+            this.from.TabIndex = 1;
+            this.from.Text = "Binary";
+            this.from.SelectedIndexChanged += new System.EventHandler(this.from_SelectedIndexChanged);
+            this.from.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.from_KeyPress);
+            // 
+            // to
+            // 
+            this.to.Enabled = false;
+            this.to.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.to.FormattingEnabled = true;
+            this.to.Items.AddRange(new object[] {
+            "Binary",
+            "Decimal",
+            "Octal",
+            "Hexadecimal"});
+            this.to.Location = new System.Drawing.Point(437, 327);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(216, 32);
+            this.to.TabIndex = 2;
+            this.to.Text = "Decimal";
+            this.to.SelectedIndexChanged += new System.EventHandler(this.to_SelectedIndexChanged);
+            this.to.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.to_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(85, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 32);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(437, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(216, 32);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // btstart
+            // 
+            this.btstart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btstart.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btstart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btstart.Location = new System.Drawing.Point(227, 423);
+            this.btstart.Name = "btstart";
+            this.btstart.Size = new System.Drawing.Size(354, 104);
+            this.btstart.TabIndex = 5;
+            this.btstart.Text = "Start Exchange";
+            this.btstart.UseVisualStyleBackColor = false;
+            this.btstart.Click += new System.EventHandler(this.btstart_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(881, 618);
+            this.Controls.Add(this.btstart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox from;
+        private System.Windows.Forms.ComboBox to;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btstart;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberingSystemProject
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum ensystem
+        {
+            ebinary, edecimal, eoctal, ehexadecimal
+        }
+        ensystem system1 = ensystem.ebinary;
+        ensystem system2 = ensystem.edecimal;
+
+        int frombtod(string binary)
+        {
+            int c = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1' || binary[i] == '0')
+                {
+                    if (binary[i] == '1')
+                        c += 1 * Convert.ToInt32(Math.Pow(2, binary.Length - 1 - i));
+                }
+                else
+                    return 0;
+
+            } return c;
+        }
+        string frombtoO(string binary)
+        {
+            int dec = frombtod(binary);
+            string oct = fromdtoo(dec.ToString());
+            return oct;
+        }
+        string fromdtob(string d)
+        {
+            StringBuilder s=new StringBuilder();
+            int reminder = 0;
+            if (d == "0" || string.IsNullOrWhiteSpace(d))
+            {
+                return "0";
+            }
+            else
+            {
+                int dec = Convert.ToInt32(d);
+                while (dec != 0)
+                {
+                    reminder = (dec % 2);
+                    dec /= 2;
+                    s.Insert(0, reminder);
+                }
+            }
+            return s.ToString().PadLeft(3, '0');
+        }
+        string fromdtoo(string dec)
+        {
+            int reminder = 0;
+            StringBuilder s = new StringBuilder();
+            
+            if (dec == "0"|| string.IsNullOrWhiteSpace(dec))
+            {
+                return "0";
+            }
+            else
+            {
+                int decc = Convert.ToInt32(dec);
+                while (decc != 0)
+                {
+                    reminder = decc % 8;
+                    decc /= 8;
+                    s.Insert(0, reminder);
+                }
+            }
+           
+            return s.ToString();
+        }
+        string fromdtohex(string dec)
+        {
+            StringBuilder s = new StringBuilder();
+            if (dec == "0" || string.IsNullOrWhiteSpace(dec))
+            {
+                return "0";
+            }
+            else
+            {
+                int d = Convert.ToInt32(dec);
+                int reminder = 0;
+                char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+                while (d > 0)
+                {
+                    reminder = d % 16;
+                    d /= 16;
+                    s.Insert(0, hex[reminder]);
+                }
+            }
+            return s.ToString();
+        }
+        string frombtohex(string binary)
+        {
+            int dec = frombtod(binary);
+            string hex = fromdtohex(dec.ToString());
+            return hex;
+        }
+        string fromotob(string octul)
+        {
+            string binary = "";
+            for(int i = 0; i < octul.Length; i++)
+            {
+                binary += fromdtob(octul[i].ToString());
+            }return binary;
+        }
+        string fromotod(string octul)
+        {
+            string binary = fromotob(octul);
+            string dec = frombtod(binary).ToString();
+            return dec;
+        }
+        string fromotohex(string octul)
+        {
+            string dec = fromotod(octul);
+            string hex = fromdtohex(dec);
+            return hex;
+        }
+        string fromhextob(string hex) 
+        {
+            string binary = "";
+            for(int i = 0; i < hex.Length; i++)
+            {
+                binary += fromdtob(hex[i].ToString()).PadLeft(4,'0');
+            }return binary;
+        }
+        string fromhextod(string hex)
+        {
+            string binary = fromhextob(hex);
+            string dec = frombtod(binary).ToString();
+            return dec;
+        }
+        string fromhextoo(string hex)
+        {
+            string dec = fromhextod(hex);
+            string octul = fromdtoo(dec);
+            return octul;
+        }
+        void generateresult()
+        {
+            if (system1 == ensystem.ebinary) {
+                switch (system2)
+                {
+                    case ensystem.edecimal:
+                        textBox2.Text = frombtod(textBox1.Text).ToString();
+                        break;
+                    case ensystem.eoctal:
+                        textBox2.Text = frombtoO(textBox1.Text);
+                        break;
+                    case ensystem.ehexadecimal:
+                        textBox2.Text = frombtohex(textBox1.Text);
+                        break;
+                    case ensystem.ebinary:
+                        textBox2.Text = textBox1.Text;
+                        break;
+                }
+
+            }
+            else if (system1 == ensystem.edecimal)
+            {
+                switch (system2)
+                {
+                    case ensystem.ebinary:
+                        textBox2.Text = fromdtob(textBox1.Text);
+                        break;
+                    case ensystem.eoctal:
+                        textBox2.Text = fromdtoo(textBox1.Text);
+                        break;
+                    case ensystem.ehexadecimal:
+                        textBox2.Text = fromdtohex(textBox1.Text);
+                        break;
+                    case ensystem.edecimal:
+                        textBox2.Text = textBox1.Text;
+                        break;
+                }
+            }
+            else if (system1 == ensystem.eoctal)
+            {
+                switch (system2)
+                {
+                    case ensystem.edecimal:
+                        textBox2.Text = fromotod(textBox1.Text);
+                        break;
+                    case ensystem.ebinary:
+                        textBox2.Text=fromotob(textBox1.Text);
+                        break;
+                    case ensystem.ehexadecimal:
+                        textBox2.Text = fromotohex(textBox1.Text);
+                        break;
+                    case ensystem.eoctal:
+                        textBox2.Text = textBox1.Text;
+                        break;
+                }
+            }
+            else
+            {
+                switch (system2)
+                {
+                    case ensystem.edecimal:
+                        textBox2.Text = fromhextod(textBox1.Text).ToString();
+                        break;
+                    case ensystem.eoctal:
+                        textBox2.Text = fromhextoo(textBox1.Text).ToString();
+                        break;
+                    case ensystem.ebinary:
+                        textBox2.Text = fromhextob(textBox1.Text).ToString();
+                        break;
+                    case ensystem.ehexadecimal:
+                        textBox2.Text = textBox1.Text;
+                        break;
+                }
+            }
+        }
+        void genaratesystem1()
+        {
+            if (from.Text == "Binary")
+            {
+                system1 = ensystem.ebinary;
+            }
+            else if (from.Text== "Decimal")
+            {
+                system1 = ensystem.edecimal;
+            }
+            else if (from.Text == "Octal")
+            {
+                system1 = ensystem.eoctal;
+            }
+            else
+            {
+                system1 = ensystem.ehexadecimal;
+            }
+        }
+        void genaratesystem2()
+        {
+            if (to.Text == "Binary")
+            {
+                system2 = ensystem.ebinary;
+            }
+            else if (to.Text == "Decimal")
+            {
+                system2 = ensystem.edecimal;
+            }
+            else if (to.Text == "Octal")
+            {
+                system2 = ensystem.eoctal;
+            }
+            else
+            {
+                system2 = ensystem.ehexadecimal;
+            }
+        }
+        private void from_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            genaratesystem1();
+        }
+        private void to_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+            genaratesystem2();
+            generateresult();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            generateresult();
+        }
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            string input = textBox1.Text;
+            if (system1 == ensystem.ebinary )
+            {
+                if (e.KeyChar == '0' || e.KeyChar == '1' || e.KeyChar == '\b')
+                {
+                    e.Handled = false;
+                    errorProvider1.SetError(textBox1, "");
+                }
+                else
+                {
+                    e.Handled = true;
+                    errorProvider1.SetError(textBox1, "accept only 0/1 ");
+                }
+            }
+            if (system1 == ensystem.ebinary) { 
+                 if (input.Length >= 31)
+                {
+                    e.Handled = true;
+                    textBox1.Text = input.Substring(0, 31);
+                    errorProvider1.SetError(textBox1, "only accept 32 digits(in binary)");
+
+                }
+            }
+            else if (system1 == ensystem.ehexadecimal)
+            {
+                if (input.Length >= 8)
+                {
+                    e.Handled = true;
+                    textBox1.Text = input.Substring(0, 8);
+                    errorProvider1.SetError(textBox1, "only accept 9 digits(in hexadecimal)");
+                }
+            }
+            else if(system1 == ensystem.eoctal)
+            {
+                if (input.Length >= 11)
+                {
+                    e.Handled = true;
+                    textBox1.Text = input.Substring(0, 11);
+                    errorProvider1.SetError(textBox1, "only accept 11 digits (in octal)");
+                }
+            }
+        }
+
+        private void btstart_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            from.Enabled = true;
+            to.Enabled = true;
+        }
+
+        private void from_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void to_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
